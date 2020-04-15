6cd1cda6b57c49b772959a1559ee5e2e05078f68
bryano13 <bryano13@hotmail.com>
2020-04-15 03:39:25 -0500

    missing files for task 5

--- execute.c
+++ execute.c
@@ -1,18 +1,13 @@
 #include "shell.h"
-
 /**
   * exec_func - Separates line into tokens and executes exceve with flags.
   * @input: Address to a string inputted by user
   * Return: Return (0)
-  *
  **/
-
 int exec_func(char *input)
 {
-
 	char **sort_tokens, **flag, **no_flag, **all_tokens;
-	int count = 0, i = 0, j = 0;
-	int found;
+	int found, count = 0, i = 0, j = 0;
 	struct stat st;
 
 	all_tokens = sep_by_space(input);
@@ -24,43 +19,30 @@ int exec_func(char *input)
 		count += count_args(no_flag);
 	}
 	sort_tokens = malloc(sizeof(char *) * count);
-
 	found = stat(all_tokens[0], &st);
 	if (found != 0)
 	{
 		perror("Error");
 		return (0);
 	}
-
 	sort_tokens[0] = malloc(_strlen(all_tokens[0]));
 	sort_tokens[0] = all_tokens[0];
-
 	if (count_args(all_tokens) > 1 && flags(all_tokens))
 	{
-		i = 1;
-		j = 0;
-		while (flag[j] != NULL)
+		for (i = 1, j = 0; flag[j] != NULL; i++, j++)
 		{
 			sort_tokens[i] = malloc(_strlen(flag[j]));
 			sort_tokens[i] = flag[j];
-			i++, j++;
 		}
-
-		j = 0;
-
-		while (no_flag[j] != NULL)
+		for (j = 0; no_flag[j] != NULL, j++, i++)
 		{
 			sort_tokens[i] = malloc(_strlen(no_flag[j]));
 			sort_tokens[i] = no_flag[j];
-			i++, j++;
 		}
 		sort_tokens[i] = NULL;
 	}
 	else
-	{
 		sort_tokens[1] = NULL;
-	}
 	execve(sort_tokens[0], sort_tokens, NULL);
-
 	return (0);
 }
---
+++ sep_by_space.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * sep_by_space - Splits a string into tokens (by space).
+ * @str: string to be splited
+ * Return: Array of tokens (splitted by spaces).
+ */
+char **sep_by_space(char *str)
+{
+
+	int len = 0;
+	char **tokens;/*Array of tokens*/
+	char *dup = _strdup(str);
+	char *token = strtok(dup, " ");/*separa la primera palabra*/
+
+	while (token != NULL)
+	{
+		token = strtok(NULL, " ");/*separa desde la segunda palabra en adelante.*/
+		len++;/*cuenta el numero de palabras en str.*/
+	}
+	tokens = malloc(sizeof(char *) * len);
+
+	put_args(tokens, str);
+
+	return (tokens);
+}
