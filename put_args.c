0fda4465cb0c62988f692324f61d7dcd12980d3b
linkjavier <linkjavier@hotmail.com>
2020-04-16 23:06:26 -0500

    Fixed put_args.c Bryan

--- put_args.c
+++ put_args.c
@@ -8,22 +8,13 @@
 char **put_args(char **arr, char *str)
 {
 	int i = 0;
-<<<<<<< HEAD
 	/*char *dup = _strdup(str)*/
 	char *token = strtok(str, " ");/*gets first string*/
-=======
-	char *dup = _strdup(str);/*Alocates space in heap for copy of str*/
-	char *token = strtok(dup, " ");/*gets first string*/
->>>>>>> refs/remotes/origin/master
 
 	while (token != NULL)
 	{
 		/*Allocate space for each string*/
-<<<<<<< HEAD
 		arr[i] = malloc(sizeof(char) * _strlen(token) + 1);
-=======
-		arr[i] = malloc(sizeof(char) * _strlen(token));
->>>>>>> refs/remotes/origin/master
 		arr[i] = token;/*Allocates string by string until NULL*/
 		token = strtok(NULL, " ");/*Moves through strings*/
 		i++;
@@ -31,9 +22,5 @@ char **put_args(char **arr, char *str)
 
 	arr[i] = NULL;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/master
 	return (arr);
 }
