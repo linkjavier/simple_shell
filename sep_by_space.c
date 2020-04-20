c1b7c4ddf147a42fd56cbafe50964b54e0debd1f
bryano13 <bryano13@hotmail.com>
2020-04-20 01:55:04 -0500

    winner

--- sep_by_space.c
+++ sep_by_space.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * sep_by_space - Splits a string into tokens (by space).
  * @str: string to be splited
@@ -7,24 +6,30 @@
  */
 char **sep_by_space(char *str)
 {
-
-	int len = 0;
-	char **tokens;/*Array of tokens*/
-	char *dup = _strdup(str);
-	char *token = strtok(dup, " ");/*separa la primera palabra*/
+	int len = 0, i = 0;
+	char *copy = _strdup(str);
+	char *token = strtok(copy, " ");/*separa la primera palabra*/
+	char **all_tokens = NULL;
 
 	while (token != NULL)
 	{
 		token = strtok(NULL, " ");/*separa desde la segunda palabra en adelante.*/
 		len++;/*cuenta el numero de palabras en str.*/
 	}
+	all_tokens = malloc(sizeof(char *) * (len + 1));
+	if (all_tokens == NULL)
+		return (NULL);
 
-		tokens = malloc(sizeof(char *) * (len + 1));
-		if (tokens == NULL)
-			return (NULL);
-		put_args(tokens, str);
+	token = strtok(str, " ");/*separa la primera palabra*/
+	while (token != NULL)
+	{
+		all_tokens[i] = malloc(sizeof(char) * _strlen(token) + 1);
+		all_tokens[i] = token;/*Allocates string by string until NULL*/
+		token = strtok(NULL, " ");/*Moves through strings*/
+		i++;
+	}
+	all_tokens[i] = NULL;
+	free(copy);
 
-	free(dup);
-
-	return (tokens);
+	return (all_tokens);
 }
--- shell.h
+++ shell.h
@@ -11,30 +11,14 @@
 #include <signal.h>
 #include <stdlib.h>
 
-/**
-  * struct directories - its used to find the PATH.
-  * @dir: directories
-  * @next: Pointer to next structure
-  *
-  * Description: directories structure
-  * for shell project.
-  */
-typedef struct directories
-{
-	char *dir;
-	struct directories *next;
-} dir_st;
 
 /*EOF function*/
 void exect_prompt(void);
 void h_signal(int sig);
-void eof_func(char c);
-void eof_1(char *phrase);
-char **put_args(char **arr, char *str);
 char **sep_by_space(char *str);
 int exec_func(char *phrase);
 int _strlen(char *str);
-char *_strcat(char *first, char *second);
 char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
 
 #endif
